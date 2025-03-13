78920f41bde9d381a774661417bfd0db5b694c0c
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-03-13 08:59:58 +0530

    feat: login api endpoint

--- server/components/users/routers/userAuth.js
+++ server/components/users/routers/userAuth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { userRegistration } = require('../controllers/');
+const { userRegistration, login } = require('../controllers/');
 const { handleValidationErrors } = require('../../../helpers/');
-const { validateUserRegistration } = require('../validators/userAuthValidation');
+const { validateUserRegistration, validateUserLogin } = require('../validators/userAuthValidation');
 
 router.post('/signUp', validateUserRegistration, handleValidationErrors, userRegistration);
+router.post('/login', validateUserLogin, handleValidationErrors, login);
 
 
 module.exports = router;
